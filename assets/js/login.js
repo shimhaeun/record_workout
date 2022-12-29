2550d6d3efebc7fb9760b26183a48b8a7d21d6d1
haeun <shy624@naver.com>
2022-12-29 17:22:20 +0900

    add lunge page, edit etc...

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,42 +1,157 @@
-$(function() {
-    // 로그인 처리
-    $('#login-submit').click(function(e) {
-        e.preventDefault();
-        if ($("#userid").val() == '') {
-            alert('아이디를 입력하세요');
-            $("#userid").focus();
-            return false;
-        }
 
-        if ($("#password").val() == '') {
-            alert('비밀번호를 입력하세요');
-            $("#password").focus();
-            return false;
-        }
+(function($) {
+    "use strict";
+	
+	// Options for Message
+	//----------------------------------------------
+  var options = {
+	  'btn-loading': '<i class="fa fa-spinner fa-pulse"></i>',
+	  'btn-success': '<i class="fa fa-check"></i>',
+	  'btn-error': '<i class="fa fa-remove"></i>',
+	  'msg-success': 'All Good! Redirecting...',
+	  'msg-error': 'Wrong login credentials!',
+	  'useAJAX': true,
+  };
 
-        $.ajax({
-            url : 'loginChk.php',
-            type : 'POST',
-            data : {
-                userid : $('#userid').val(),
-                password : $('#password').val()
-            },
-            dataType : "json",
-            success : function(response) {
-                if (response.result == 1) {
-                    //alert('로그인 성공');
-                    location.replace('../index.php'); // 화면 갱신
-                    //location.reload(); // 화면 갱신
-                } else if (response.result == -2) {
-                    alert('입력된 값이 없습니다');
-                } else {
-                    alert('로그인 실패');
-                }
-            },
-            error : function(jqXHR, textStatus, errorThrown) {
-                alert("arjax error : " + textStatus + "\n" + errorThrown);
-            }
-        });
-    });
-    
-});
+	// Login Form
+	//----------------------------------------------
+	// Validation
+  $("#login-form").validate({
+  	rules: {
+      lg_username: "required",
+  	  lg_password: "required",
+    },
+  	errorClass: "form-invalid"
+  });
+  
+	// Form Submission
+  $("#login-form").submit(function() {
+  	remove_loading($(this));
+		
+		if(options['useAJAX'] == true)
+		{
+			// Dummy AJAX request (Replace this with your AJAX code)
+		  // If you don't want to use AJAX, remove this
+  	  dummy_submit_form($(this));
+		
+		  // Cancel the normal submission.
+		  // If you don't want to use AJAX, remove this
+  	  return false;
+		}
+  });
+	
+	// Register Form
+	//----------------------------------------------
+	// Validation
+  $("#register-form").validate({
+  	rules: {
+      reg_username: "required",
+  	  reg_password: {
+  			required: true,
+  			minlength: 5
+  		},
+   		reg_password_confirm: {
+  			required: true,
+  			minlength: 5,
+  			equalTo: "#register-form [name=reg_password]"
+  		},
+  		reg_email: {
+  	    required: true,
+  			email: true
+  		},
+  		reg_agree: "required",
+    },
+	  errorClass: "form-invalid",
+	  errorPlacement: function( label, element ) {
+	    if( element.attr( "type" ) === "checkbox" || element.attr( "type" ) === "radio" ) {
+    		element.parent().append( label ); // this would append the label after all your checkboxes/labels (so the error-label will be the last element in <div class="controls"> )
+	    }
+			else {
+  	  	label.insertAfter( element ); // standard behaviour
+  	  }
+    }
+  });
+
+  // Form Submission
+  $("#register-form").submit(function() {
+  	remove_loading($(this));
+		
+		if(options['useAJAX'] == true)
+		{
+			// Dummy AJAX request (Replace this with your AJAX code)
+		  // If you don't want to use AJAX, remove this
+  	  dummy_submit_form($(this));
+		
+		  // Cancel the normal submission.
+		  // If you don't want to use AJAX, remove this
+  	  return false;
+		}
+  });
+
+	// Forgot Password Form
+	//----------------------------------------------
+	// Validation
+  $("#forgot-password-form").validate({
+  	rules: {
+      fp_email: "required",
+    },
+  	errorClass: "form-invalid"
+  });
+  
+	// Form Submission
+  $("#forgot-password-form").submit(function() {
+  	remove_loading($(this));
+		
+		if(options['useAJAX'] == true)
+		{
+			// Dummy AJAX request (Replace this with your AJAX code)
+		  // If you don't want to use AJAX, remove this
+  	  dummy_submit_form($(this));
+		
+		  // Cancel the normal submission.
+		  // If you don't want to use AJAX, remove this
+  	  return false;
+		}
+  });
+
+	// Loading
+	//----------------------------------------------
+  function remove_loading($form)
+  {
+  	$form.find('[type=submit]').removeClass('error success');
+  	$form.find('.login-form-main-message').removeClass('show error success').html('');
+  }
+
+  function form_loading($form)
+  {
+    $form.find('[type=submit]').addClass('clicked').html(options['btn-loading']);
+  }
+  
+  function form_success($form)
+  {
+	  $form.find('[type=submit]').addClass('success').html(options['btn-success']);
+	  $form.find('.login-form-main-message').addClass('show success').html(options['msg-success']);
+  }
+
+  function form_failed($form)
+  {
+  	$form.find('[type=submit]').addClass('error').html(options['btn-error']);
+  	$form.find('.login-form-main-message').addClass('show error').html(options['msg-error']);
+  }
+
+	// Dummy Submit Form (Remove this)
+	//----------------------------------------------
+	// This is just a dummy form submission. You should use your AJAX function or remove this function if you are not using AJAX.
+//   function dummy_submit_form($form)
+//   {
+//   	if($form.valid())
+//   	{
+//   		form_loading($form);
+  		
+//   		setTimeout(function() {
+//   			form_success($form);
+//   		}, 2000);
+//   	}
+//   }
+	
+// })(jQuery);
